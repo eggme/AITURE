5c04daf927dd07b981d9fce7fe2533b8b0944d0c
kyyet <kyyeto9984@naver.com>
2018-07-23 19:53:02 +0900

    푸쉬 테스트

--- app/src/main/java/com/lsj/aiture/GPSController.java
+++ app/src/main/java/com/lsj/aiture/GPSController.java
@@ -9,7 +9,7 @@ public class GPSController {
     // GPS를 이용하여 현재 주소, 위도, 경도를 반환
 
     public int getLatitude(){
-        return 59;
+        return 59; // ㅇㅇ
     }
 
     public int getLontitude(){

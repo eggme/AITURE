7b5122f3ea72e5b6eba98a6805f1f22b1750754e
kyyet <kyyeto9984@naver.com>
2018-08-06 22:29:46 +0900

    블루투스 수정

--- app/src/main/java/com/lsj/aiture/BluetoothService.java
+++ app/src/main/java/com/lsj/aiture/BluetoothService.java
@@ -1,59 +1,235 @@
 package com.lsj.aiture;
 
-import android.app.Activity;
-import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.os.Handler;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
-public class BluetoothService extends Service {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
-    private final String TAG = "BluetoothService";
-    private final int REQUEST_ENABLE_BLUETOOTH = 1;
+public class BluetoothService {
 
-    private BluetoothAdapter btAdapter;
-    private Activity activity;
+    private static final String TAG = "BluetoothSerivce";
+    private static final UUID MY_UUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
+
+    private BluetoothAdapter adapter;
     private Handler handler;
+    // 연결대기 스레드
+    private AcceptThread accpetThread;
+    // 연결 스레드
+    private ConnectThread connectThread;
+    // 연결중 스레드
+    private ConnectedThread connectedThread;
+    private int state;
 
-    public BluetoothService(Activity activity, Handler handler){
-        this.activity = activity;
+    // 상태
+    public static final int STATE_NONE = 0;
+    public static final int STATE_LISTEN = 1;
+    public static final int STATE_CONNECTING = 2;
+    public static final int STATE_CONNECTED = 3;
+
+
+    public BluetoothService(BluetoothAdapter adapter, Handler handler){
+        state = STATE_NONE;
+        this.adapter = adapter;
         this.handler = handler;
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
-    public boolean getDeviceState(){
-        Log.i(TAG,"Check the Bluetooth Support");
+    // 상태 설정
+    private synchronized void setState(int state){
+        this.state = state;
+    }
 
-        if(btAdapter == null){
-            Log.i(TAG,"Bluetooth is not available");
-            return false;
-        }else{
-            Log.i(TAG,"Bluetooth is available");
-            return true;
+    //상태값 리턴
+    public synchronized int getState(){
+        return state;
+    }
+
+    // 전송 시작
+    public synchronized void start(){
+        if(connectedThread != null){
+            connectedThread = null;
+        }
+
+        if(accpetThread == null){
+            accpetThread = new AcceptThread();
+            accpetThread.start();
+        }
+        setState(STATE_LISTEN);
+    }
+
+    // 블루투스 연결
+    public synchronized void connect(BluetoothDevice device){
+        if(state == STATE_CONNECTING){
+            if(connectThread != null){connectThread.cancle();connectThread = null;}
+        }
+        if(connectedThread != null){connectedThread.cancle();connectedThread = null;}
+
+        connectThread = new ConnectThread(device);
+        connectThread.start();
+        setState(STATE_CONNECTED);
+    }
+
+    public synchronized void connected(BluetoothSocket socket, BluetoothDevice device){
+
+        if(connectThread != null){connectThread.cancle();connectThread = null;}
+        if(connectedThread != null){connectedThread.cancle();connectedThread = null;}
+        if(accpetThread != null){accpetThread.cancle();accpetThread = null;}
+
+        connectedThread = new ConnectedThread(socket);
+        connectedThread.start();
+
+        setState(STATE_CONNECTED);
+    }
+
+    public synchronized void stop(){
+        if(connectThread != null){connectThread.cancle();connectThread = null;}
+        if(connectedThread != null){connectedThread.cancle();connectedThread = null;}
+        if(accpetThread != null){accpetThread.cancle();accpetThread = null;}
+
+        setState(STATE_NONE);
+    }
+
+    public void write(byte[] out){
+        ConnectedThread r;
+        synchronized (this){
+            if(state != STATE_CONNECTED) return;
+            r= connectedThread;
+        }
+        r.write(out);
+    }
+
+    private void connectionLost(){
+        setState(STATE_LISTEN);
+    }
+
+    private void connectionFailed(){
+        setState(STATE_LISTEN);
+    }
+
+
+    private class AcceptThread extends Thread{
+
+        private BluetoothServerSocket serverSocket;
+
+        public AcceptThread(){
+        }
+
+        @Override
+        public void run() {
+            BluetoothSocket socket;
+            try{
+                serverSocket = adapter.listenUsingInsecureRfcommWithServiceRecord(TAG,MY_UUID);
+                socket = serverSocket.accept();
+                if(socket != null){
+                    connected(socket, socket.getRemoteDevice());
+                }
+            }catch (IOException e){
+                Log.i(TAG, "listen 실패");
+            }
+        }
+
+        public void cancle(){
+            try{
+                serverSocket.close();
+            }catch (IOException e){Log.i(TAG, "AcceptThread close 에러");}
         }
     }
 
-    public void enableBluetooth(){
-        Log.i(TAG,"Check the enable Bluetooth");
+    private class ConnectThread extends Thread{
 
-        if(btAdapter.isEnabled()){
-            Log.i(TAG, "Bluetooth Enable Now");
-        }else{
-            Log.i(TAG, "Bluetooth Enable Request");
+        private BluetoothDevice device;
+        private BluetoothSocket socket;
 
-            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            activity.startActivityForResult(intent, REQUEST_ENABLE_BLUETOOTH);
+        private ConnectThread(BluetoothDevice device){
+            this.device = device;
+            BluetoothSocket tmp = null;
+
+            try{
+                tmp = device.createInsecureRfcommSocketToServiceRecord(MY_UUID);
+            }catch (IOException e){Log.i(TAG, "ConnectThread create 에러");}
+            socket = tmp;
+        }
+
+        @Override
+        public void run() {
+            adapter.cancelDiscovery();
+
+            try{
+                socket.connect();
+            }catch (IOException e){
+                connectionFailed();
+                try{
+                    socket.close();
+                }catch (IOException e1){Log.i(TAG, "ConnectThread connection failure 에러");}
+
+                BluetoothService.this.start();
+                return;
+            }
+
+            synchronized (BluetoothService.this){
+                connectThread = null;
+            }
+
+            connected(socket, device);
+        }
+
+        public void cancle(){
+            try{
+                socket.close();
+            }catch (IOException e){Log.i(TAG, "ConnectThread close 에러");}
         }
     }
 
+    private class ConnectedThread extends Thread{
 
+        private BluetoothSocket socket;
+        private InputStream inputStream;
+        private OutputStream outputStream;
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+        private ConnectedThread(BluetoothSocket socket){
+            this.socket = socket;
+            try{
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+            }catch (IOException e){Log.i(TAG, "ConnectedThread socket not created 에러");}
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true){
+                try{
+                    bytes = inputStream.read(buffer);
+
+                    handler.obtainMessage(BluetoothState.BLUETOOTH_MESSAGE_READ ,bytes, -1, buffer).sendToTarget();
+                }catch (IOException e){
+                    Log.i(TAG, "disconnected 에러");
+                    connectionLost();
+                    break;
+                }
+            }
+        }
+
+        public void cancle(){
+            try{
+                socket.close();
+            }catch (IOException e){Log.i(TAG, "ConnectedThread cancle 에러");}
+        }
+
+        public void write(byte[] buffer){
+            try{
+                outputStream.write(buffer);
+                handler.obtainMessage(BluetoothState.BLUETOOTH_MESSAGE_WRITE, -1, -1, buffer);
+            }catch (IOException e){Log.i(TAG,"write Error 에러");}
+        }
     }
+
 }
